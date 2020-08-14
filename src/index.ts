bc3b0e2b86f5b07d4669cb5b98986306d49afa2b
cuvee <cuvee@cuveedeMacBook-Pro.local>
2020-08-14 10:50:32 +0800

    feat: 新增create和use接口

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,59 @@
-import axios, { AxiosInstance } from 'axios';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import axios, {
+  AxiosInterceptorManager,
+  AxiosRequestConfig,
+  AxiosResponse,
+  AxiosInstance,
+} from 'axios';
 
-const ins: AxiosInstance = axios.create({
-  baseURL: 'http://127.0.0.1:3001/',
-});
+import baseConfig from './config';
 
-ins.interceptors.request.use(
-  (config) => {
-    console.log(config);
-    return config;
-  },
-  (e) => {
-    console.log(e);
-    return Promise.reject(e);
+export type IMiddleFn = (
+  req: AxiosInterceptorManager<AxiosRequestConfig>,
+  res: AxiosInterceptorManager<AxiosResponse>
+) => void;
+
+let middlesQueue: any[] = [];
+
+const _toString = Object.prototype.toString;
+/**
+ * @param fn
+ */
+export function use(fn: IMiddleFn | IMiddleFn[]): IMiddleFn[] | undefined {
+  if (!isFunction(fn) && !Array.isArray(fn)) {
+    warn(
+      `If you want to register middleware through use,` +
+        `when calling use, please pass in the function or array type as the parameter`
+    );
+    return;
   }
-);
+  middlesQueue.push(fn);
+  middlesQueue = flat(middlesQueue);
 
-ins.interceptors.response.use(
-  (config) => {
-    console.log(config);
-    return config;
-  },
-  (e) => {
-    console.log(e);
-    return Promise.reject(e);
-  }
-);
+  return middlesQueue as IMiddleFn[];
+}
 
-export { ins };
+export function create(config: AxiosRequestConfig): AxiosInstance {
+  config = Object.assign({}, config, baseConfig);
+  const ins = axios.create(config);
+
+  middlesQueue.forEach((middleware: IMiddleFn) => {
+    middleware.call(null, ins.interceptors.request, ins.interceptors.response);
+  });
+
+  return ins;
+}
+
+function isFunction(varaiable: any): boolean {
+  return _toString.call(varaiable) === '[object Function]';
+}
+
+function flat<T>(arr: T[]): T[] {
+  return arr.reduce((prev: T[], cur: T) => {
+    return Array.isArray(cur) ? [...prev, ...flat([...cur])] : [...prev, cur];
+  }, []);
+}
+
+export function warn(info: string): void {
+  console.warn(info);
+}
